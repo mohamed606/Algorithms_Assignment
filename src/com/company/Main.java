59a5c93c6c0ff299e5976b1bd2ec1f852c381f24
Abdelrhman199 <abdelrhman_hosny1212@yahoo.com>
2020-02-29 19:40:51 +0200

    Second problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	Tree<Integer> tree=new Tree<>(5);
+	tree.setLeft(new Tree<>(6));
+	tree.setRight(new Tree<>(7));
+	tree.getLeft().setLeft(new Tree<>(3));
+		Integer [] arr =tree.largestValues(tree);
+		for (int i=0;i<arr.length;i++)
+		{
+			System.out.println("number : "+arr[i]);
+		}
+
+
     }
 
 }
--- src/com/company/Tree.java
+++ src/com/company/Tree.java
@@ -1,11 +1,70 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Tree<T> {
     private T value;
     private Tree<T> left;
     private Tree<T> right;
     public Tree(T value){
         this.value = value;
+    }
+
+    public Integer [] largestValues(Tree<Integer> t)
+    {
+        ArrayList<Integer> arrayList = getLevelMaxNumber(t);
+        return arrayList.toArray(new Integer[0]);
+    }
+    public int getHeight(Tree<Integer> root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int leftHeight=getHeight(root.getLeft());
+        int rightHeight=getHeight(root.getRight());
+        if (leftHeight>rightHeight)
+        {
+            return leftHeight+1;
+        }
+        else
+        {
+            return rightHeight+1;
+        }
+    }
+    public ArrayList<Integer> getLevelMaxNumber(Tree<Integer>root)
+    {
+        int height=getHeight(root);
+        int max=0;
+        ArrayList<Integer>levelNumbers=new ArrayList<>();
+        ArrayList<Integer> maxNumbers= new ArrayList<>();
+
+        System.out.println("height " +height);
+        for(int i=1;i<=height;i++)
+        {
+            printGivenLevel(root,i,levelNumbers);
+            maxNumbers.add(Collections.max(levelNumbers));
+            levelNumbers.clear();
+        }
+        return maxNumbers;
+    }
+    public void printGivenLevel(Tree<Integer> root, int level, ArrayList<Integer> numbersInLevel)
+    {
+        if(root==null)
+        {
+            return ;
+        }
+        if(level==1)
+        {
+            numbersInLevel.add(root.getValue());
+        }
+        else if (level>1)
+        {
+            printGivenLevel(root.getLeft(),level-1,numbersInLevel);
+            printGivenLevel(root.getRight(),level-1,numbersInLevel);
+        }
+
     }
 
     public T getValue() {
