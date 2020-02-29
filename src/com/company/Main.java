74b3b0a433313bcfc00a51a706f023b59b76f4fb
mohamed606 <mohamed606tanna@gmail.com>
2020-02-29 12:16:29 +0200

    Adding isSymmetric and isSymmetricRec

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Tree<Integer>tree = new Tree<>(1);
+        tree.setLeft(new Tree<>(2));
+        tree.setRight(new Tree<>(2));
+        tree.getLeft().setLeft(new Tree<>(3));
+        tree.getLeft().setRight(new Tree<>(4));
+        tree.getRight().setLeft(new Tree<>(4));
+        tree.getRight().setRight(new Tree<>(3));
+//        Tree<Integer>tree = new Tree<>(1);
+//        tree.setLeft(new Tree<>(2));
+//        tree.setRight(new Tree<>(2));
+//        tree.getLeft().setRight(new Tree<>(3));
+//        tree.getLeft().setLeft(new Tree<>(3));
+//        tree.getRight().setRight(new Tree<>(4));
+//        tree.getRight().setLeft(new Tree<>(4));
+//        Tree<Integer>tree = new Tree<>(1);
+//        tree.setLeft(new Tree<>(2));
+//        tree.setRight(new Tree<>(2));
+//        tree.getLeft().setRight(new Tree<>(3));
+//        tree.getRight().setRight(new Tree<>(3));
+        System.out.println("isSymmetric "+tree.isSymmetric(tree));
+        System.out.println("isSymmetricRec "+tree.isSymmetricRecurs(tree));
     }
 
 }
